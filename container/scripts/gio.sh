53d4c7df82ab9a74a86f04d040d2844ff2a7c25b
Mateus Souza <mateussouzaweb@gmail.com>
2026-09-18 18:08:00 -0300

    Fix: use correct command and verification for container scripts

--- container/scripts/gio.sh
+++ container/scripts/gio.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
-if [ -x "/usr/local/bin/lxg" ] && [ "$1" = "open" ]; then
-    exec /usr/local/bin/lxg bridge exec gio "$@"
+if [ -n "$LXG_CONTAINER" ] && \
+   [ -x "/usr/local/bin/lxg" ] && \
+   [ "$1" = "open" -o "$1" = "launch" ]; }; then
+    exec /usr/local/bin/lxg container request gio "$@"
 else
     exec /usr/bin/gio "$@"
 fi
--- container/scripts/xdg-open.sh
+++ container/scripts/xdg-open.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-if [ -x "/usr/local/bin/lxg" ]; then
+if [ -n "$LXG_CONTAINER" ] && \
+   [ -x "/usr/local/bin/lxg" ]; then
     exec /usr/local/bin/lxg container request xdg-open "$@"
 else
     exec /usr/bin/xdg-open "$@"
